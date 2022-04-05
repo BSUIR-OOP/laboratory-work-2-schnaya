242265d9582840fd9e14375ddf896df7b4a7600d
schnaya <76622822+schnaya@users.noreply.github.com>
2022-04-05 23:21:01 +0300

    Add files via upload

---
+++ Line.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Line
+    {
+        public virtual void Func()
+        {
+        }
+    }
+}
